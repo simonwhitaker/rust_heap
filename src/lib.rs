ce07e5d9411432a3ab8d9e2b37010f171a7c2e9d
Simon Whitaker <sw@netcetera.org>
2017-07-30 22:34:13 +0100

    Add basic add/top methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,34 @@
+pub struct Heap<T> {
+    storage: Vec<T>,
+}
+
+impl <T> Heap<T> {
+    pub fn new() -> Self {
+        let mut _storage = Vec::new();
+        return Heap { storage: _storage };
+    }
+
+    fn add(&mut self, element: T) {
+        self.storage.push(element);
+    }
+
+    fn top(&self) -> Option<&T> {
+        return self.storage.get(0);
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use Heap;
+
     #[test]
-    fn it_works() {
+    fn test_add_and_get() {
+        let mut heap: Heap<i32> = Heap::new();
+        let val: i32 = 42;
+
+        heap.add(val.clone());
+        let top = heap.top().expect("Ugh");
+
+        assert_eq!(*top, val);
     }
 }
